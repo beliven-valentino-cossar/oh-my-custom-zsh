0ba9cc83dd8b71af672d4ba391449c20ebb67ae0
Valentino Cossar <4087384+valentinocossar@users.noreply.github.com>
2025-07-24 14:16:58 +0200

    fix: dnsmasq installer

--- dnsmasq/installer.sh
+++ dnsmasq/installer.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-brew install dnsmasq
-sed -i '' -e 's/#listen-address=/listen-address=127.0.0.1/g' $HOMEBREW_PREFIX/etc/dnsmasq.conf
-sed -i '' -e 's/#address=\/double-click.net\/127.0.0.1/address=\/.test\/127.0.0.1/g' $HOMEBREW_PREFIX/etc/dnsmasq.conf
-sudo mkdir -pv /etc/resolver
-sudo bash -c 'echo "nameserver 127.0.0.1" > /etc/resolver/test'
-sudo brew services restart dnsmasq
+if [[ -z "${HOMEBREW_PREFIX}" ]]; then
+  echo "HOMEBREW_PREFIX environment variable not found. Set it accordingly."
+else
+  brew install dnsmasq
+  mkdir -p $HOMEBREW_PREFIX/etc/dnsmasq.d
+  echo -e "address=/test/127.0.0.1\nlisten-address=127.0.0.1\ninterface=lo0" > $HOMEBREW_PREFIX/etc/dnsmasq.d/test.conf
+  sudo mkdir -pv /etc/resolver
+  sudo bash -c 'echo "nameserver 127.0.0.1" > /etc/resolver/test'
+  sudo brew services restart dnsmasq
+fi
